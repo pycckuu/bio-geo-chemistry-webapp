d94e73bad13b593beb3958a11f203e9c551e4af4
pycckuu <is.markelov@gmail.com>
2015-06-15 13:05:38 +0200

    Arsenic

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   match '/error' => 'pages#error', via: [:get, :post], as: 'error_page'
   get '/terms' => 'pages#terms', as: 'terms'
   get '/privacy' => 'pages#privacy', as: 'privacy'
+  get '/as' => 'pages#as', as: 'Arsenic'
 
   # OAuth
   oauth_prefix = Rails.application.config.auth.omniauth.path_prefix
