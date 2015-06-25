8e0918d61958b189615dcdcbe6f55d297c0b405b
pycckuu <is.markelov@gmail.com>
2015-06-25 10:31:34 +0200

    about in footer and compiling css at production

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   match '/error' => 'pages#error', via: [:get, :post], as: 'error_page'
   get '/terms' => 'pages#terms', as: 'terms'
   get '/privacy' => 'pages#privacy', as: 'privacy'
+  get '/about' => 'pages#about', as: 'about'
   get '/as' => 'pages#as', as: 'Arsenic'
   get '/bioenergetics' => 'pages#bioenergetics', as: 'bioenergetics'
 
